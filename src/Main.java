eefb28a37688382acc8b06cbbcfbe666681b0d93
elliebennings <benninej@mail.uc.edu>
2025-09-18 13:21:21 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)  {
+
+     double springCost = 100;
+     double summerCost = 200;
+     double fallCost = 150;
+     double winterCost = 300;
+
+     double totalYearlyCost = summerCost + springCost + fallCost + winterCost;
+
+    }
+}
